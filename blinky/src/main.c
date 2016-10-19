d3fdceda3325476ed308d01970bcce6575e7e3d6
Jason <jasonaschmidt1224@gmail.com>
2016-10-19 18:59:13 -0400

    Clock still screwed up

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -11,36 +11,41 @@ void usart_init()
   GPIOA->MODER |= GPIO_MODER_MODER2_1 | GPIO_MODER_MODER3_1;
   GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR2 | GPIO_OSPEEDER_OSPEEDR3;
   RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
-  USART2->BRR  = 0x96; //20000000L/(2*8*115200L);
+  USART2->BRR  = 0x96;
   USART2->CR1 |= (USART_CR1_RE | USART_CR1_TE);
   USART2->CR1 |= USART_CR1_UE;
 }
 
 void clk_init()
 {
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  /*RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
   GPIOA->AFR[0] &= ~0xF;
   GPIOA->MODER |= GPIO_MODER_MODER8_1;
 
   RCC->CFGR |= RCC_CFGR_MCO1;
-  RCC->CFGR &= RCC_CFGR_MCO1PRE; //prescale x 5
-
+  RCC->CFGR |= RCC_CFGR_MCO1PRE; //prescale x 5
+*/
   RCC->CR |= RCC_CR_HSEON;  //Enable external oscillator
   while((RCC->CR & RCC_CR_HSERDY) != RCC_CR_HSERDY);
 
-  //Set HSE oscillator as PLL source, set PLLP to 4, PLLN to 80,
-  //and PLLM to 8 so that VCO output is 20 MHz
-  RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC | RCC_PLLCFGR_PLLP_0 |
-                  RCC_PLLCFGR_PLLN_4 | RCC_PLLCFGR_PLLN_6 |RCC_PLLCFGR_PLLM_3;
+
+  //RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE | RCC_PLLCFGR_PLLP_0 | RCC_PLLCFGR_PLLN_2 |
+  //                RCC_PLLCFGR_PLLN_5 | RCC_PLLCFGR_PLLN_6 | RCC_PLLCFGR_PLLM_1;
+
+  RCC->PLLCFGR = (2 << 28) | RCC_PLLCFGR_PLLSRC_HSE | (4 << 0) | (50 << 6)
+               | (0 << 16);
+
 
   RCC->CR |= RCC_CR_PLLON;
-  while((RCC->CR & RCC_CR_PLLRDY) != RCC_CR_PLLRDY);
+  GPIOD->BSRR = GPIO_BSRR_BR_12;
+  while(!(RCC->CR & RCC_CR_PLLRDY));
 
   //Select System clock
   RCC->CFGR &= ~RCC_CFGR_SW;
   RCC->CFGR |= RCC_CFGR_SW_PLL;
 
-  while((RCC->CFGR & RCC_CFGR_SW_PLL) != RCC_CFGR_SW_PLL);
+  while(!(RCC->CFGR & RCC_CFGR_SW_PLL));
+  GPIOD->BSRR = GPIO_BSRR_BR_13;
 }
 
 int send_char(int ch)
@@ -106,7 +111,7 @@ void delay()
     uint32_t i;
 
     counter = 0;
-    for(i = 0; i < 0xFFFF; i++) {
+    for(i = 0; i < 0xFFFFF; i++) {
         counter = i;
         counter = counter;
     }
@@ -123,19 +128,20 @@ int EXTI0_IRQHandler()
 
 int main()
 {
-    clk_init();
     led_init();
-    button_init();
-    exti_init();
-    usart_init();
-    GPIOD->BSRR = GPIO_BSRR_BR_12;
-    led_update(counter);
+    clk_init();
+
+    //button_init();
+    //exti_init();
+    //usart_init();
+    //GPIOD->BSRR = GPIO_BSRR_BR_12;
+    //led_update(counter);
     while(1) {
-        send_char('U');
-        //delay();
-        //counter += 1;
-        led_update(counter);
-        //SendChar('U');
+    //    led_update(counter);
+    GPIOD->BSRR = GPIO_BSRR_BS_12;
+    //delay();
+    //GPIOD->BSRR = GPIO_BSRR_BR_12;
+    //delay();
     }
     return 0;
 }
