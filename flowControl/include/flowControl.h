c90910af765b6142c6af483466cf0bb6f1dd3951
Bridget <bridget.bissell@gmail.com>
2016-12-07 18:04:24 -0500

    ADded flow control files

--- flowControl/include/flowControl.h
+++ flowControl/include/flowControl.h
@@ -1,7 +1,8 @@
 #ifndef __FLOWCONTROL_H
 #define __FLOWCONTROL_H
 
-void flowControl_init();
+void flowControl_TXinit();
+void flowControl_RXinit();
 int flowControl_transmit(uint8_t *msg, uint16_t len);
 int flowControl_receive(uint8_t *buf, uint16_t *len);
 
--- flowControl/src/flowControl.c
+++ flowControl/src/flowControl.c
@@ -3,12 +3,36 @@
 #include <stdint.h>
 #include <math.h>
 #include "flowControl.h"
-#include "flowControl.h"
+#include "stm32f407xx.h"
+#include "stm32f4xx_hal_dma.h"
 
+static DMA_HandleTypeDef DMA_HandleStructure;
 
-void flowControl_init()
+void flowControl_TXinit()
 {
-  //DMA stuff here
+  //DMA config
+  //Stream6, TX
+  DMA_HandleStructure.Instance = DMA1_Stream6;
+  DMA_HandleStructure.Init.Channel = DMA_Channel_4;   //DMA_HandleTypeDef has DMA_InitTypeDef in it
+  DMA_HandleStructure.Init.Direction = DMA_MEMORY_TO_PERIPH;
+  DMA_HandleStructure.Init.PeriphInc = DMA_PINC_DISABLE;
+  DMA_HandleStructure.Init.MemInc = DMA_MINC_DISABLE;
+  DMA_HandleStructure.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+  DMA_HandleStructure.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+  DMA_HandleStructure.Init.Mode = DMA_NORMAL;
+  DMA_HandleStructure.Init.Priority = DMA_PRIORITY_HIGH;
+  DMA_HandleStructure.Init.FIFOMode = DMA_FIFOMODE_DISABLE; //We don't care about out of order data
+
+  HAL_DMA_Init(&DMA_HandleStructure);
+}
+
+void flowControl_RXinit()
+{
+  //Stream5, RX
+  DMA_HandleStructure.Instance = DMA1_Stream6;
+  DMA_HandleStructure.Init.Channel = DMA_Channel_4;   //DMA_HandleTypeDef has DMA_InitTypeDef in it
+  DMA_HandleStructure.Init.Direction = DMA_PERIPH_TO_MEMORY;
+
 }
 
 int flowControl_transmit(uint8_t *msg, uint16_t len)
